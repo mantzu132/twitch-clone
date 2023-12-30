49ee759f51c2ff29268577c6ea3866467123ef8a
Mantas <defixass@gmail.com>
2023-12-30 20:49:13 +0200

    shadcn dark mode + clerk dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +18,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="gamehub-theme"
+          >
+            <ClerkProvider appearance={{ baseTheme: dark }}>
+              {children}
+            </ClerkProvider>
+          </ThemeProvider>
+        </body>
       </html>
-    </ClerkProvider>
+    </>
   );
 }
