1b882d69c7d5431d1241c7186568d56939d1830c
Mantas <defixass@gmail.com>
2024-01-02 21:00:57 +0200

    set up prisma and db

---
+++ lib/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+let db;
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  db = global.prisma;
+}
+
+export default db;
