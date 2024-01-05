2ab213203651b0ce55ca24a8bea6523d3bc88515
Mantas <defixass@gmail.com>
2024-01-05 13:50:25 +0200

    searchbar finished

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,6 +1,59 @@
 "use client";
-const Search = () => {
-  return <div>Searchbar Hello!</div>;
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Search, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import qs from "query-string";
+
+const SearchBar = () => {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!value) return;
+    const url = qs.stringifyUrl(
+      {
+        url: "/search",
+        query: { term: value },
+      },
+      {
+        skipEmptyString: true,
+      },
+    );
+    router.push(url);
+  };
+
+  const onClear = () => {
+    setValue("");
+  };
+  return (
+    <form
+      className="relative flex w-full lg:w-[400px] items-center rounded-r-none"
+      onSubmit={onSubmit}
+    >
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+      />
+      {value && (
+        <X
+          onClick={onClear}
+          className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+        />
+      )}
+      <Button
+        type="submit"
+        size="sm"
+        variant="secondary"
+        className="rounded-l-none"
+      >
+        <Search className="h-5 w-5 text-muted-foreground" />
+      </Button>
+    </form>
+  );
 };
 
-export default Search;
+export default SearchBar;
